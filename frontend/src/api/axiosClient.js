131dba89bd2ce3c341cb722edcf56ecfa1f7d272
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2025-12-06 19:50:12 +0530

    Update axiosClient.js

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,3 +1,3 @@
 import axios from "axios";
-const base = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
+const base = import.meta.env.VITE_BACKEND_URL || "https://studysphere-rqsc.onrender.com";
 export default axios.create({ baseURL: base });
